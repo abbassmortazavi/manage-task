616923590b8df116dcee09f69624f6e800aecd51
Abbass <abbassmortazavi@gmail.com>
2024-12-04 20:29:04 +0330

    create folder structure

---
+++ core/view.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * view.php
+ * @author Abbass Mortazavi <abbassmortazavi@gmail.com | Abbass Mortazavi>
+ * @copyright Copyright &copy; from assessment
+ * @version 1.0.0
+ * @date 2024/12/04 20:27
+ */
